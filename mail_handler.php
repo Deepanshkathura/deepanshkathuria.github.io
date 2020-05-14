f532cc8ea50998df199a6ba3d17492e99985ecd0
Deepanshkathura <65365227+Deepanshkathura@users.noreply.github.com>
2020-05-15 01:34:01 +0530

    My first commit!

---
+++ mail_handler.php
@@ -0,0 +1,22 @@
+<?php
+
+
+	   
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+	    $msg=$_POST['msg'];
+		
+		$to='deepanshkathuria@ymail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form submission';
+		$message="Name :".$name."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!		</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	
+	echo "------------------------------------";
+?>
